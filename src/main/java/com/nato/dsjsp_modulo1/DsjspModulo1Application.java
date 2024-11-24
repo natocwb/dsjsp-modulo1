5d357f61e2e7c8b9a4688ae38071b16c48beaad2
natalicio almeida <natoctba@hotmail.com>
2024-11-24 11:59:35 -0300

    init

---
+++ src/main/java/com/nato/dsjsp_modulo1/DsjspModulo1Application.java
@@ -0,0 +1,14 @@
+package com.nato.dsjsp_modulo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsjspModulo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsjspModulo1Application.class, args);
+	}
+	
+
+}
---
+++ src/test/java/com/nato/dsjsp_modulo1/DsjspModulo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nato.dsjsp_modulo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsjspModulo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
